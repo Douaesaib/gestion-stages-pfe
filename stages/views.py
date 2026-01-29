d362ef932126608b195c2b698685a75ceee56f55
chaimae.chidmi <chaimaechidmi@gmail.com>
2026-01-30 00:14:07 +0100

    Update views.py

--- stages/views.py
+++ stages/views.py
@@ -1,12 +1,9 @@
-# Create your views here.
-from django.shortcuts import render
-from .models import Offre
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Offre, Candidature
 
 def offres_list(request):
     offres = Offre.objects.select_related("entreprise").all().order_by("-id")
     return render(request, "stages/offres_list.html", {"offres": offres})
-    from django.shortcuts import render, get_object_or_404, redirect
-from .models import Offre, Candidature
 
 def postuler(request, offre_id):
     offre = get_object_or_404(Offre, id=offre_id)
@@ -14,11 +11,7 @@ def postuler(request, offre_id):
     if request.method == "POST":
         nom = request.POST.get("nom_stagiaire", "").strip()
         if nom:
-            Candidature.objects.create(
-                offre=offre,
-                nom_stagiaire=nom
-            )
+            Candidature.objects.create(offre=offre, nom_stagiaire=nom)
             return redirect("stages:offres_list")
 
     return render(request, "stages/postuler.html", {"offre": offre})
-
