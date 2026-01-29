b1bf8289495b297f6795168f2b8802e45d4e3715
chaimae.chidmi <chaimaechidmi@gmail.com>
2026-01-29 23:14:04 +0100

    Update models.py

--- stages/models.py
+++ stages/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Entreprise(models.Model):
+    nom = models.CharField(max_length=255)
+    adresse = models.TextField()
+    secteur = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nom
+
+
+class Offre(models.Model):
+    entreprise = models.ForeignKey(Entreprise, on_delete=models.CASCADE)
+    titre = models.CharField(max_length=255)
+    description = models.TextField()
+    date_debut = models.DateField()
+    date_fin = models.DateField()
+
+    def __str__(self):
+        return self.titre
+
+
+class Candidature(models.Model):
+    nom_etudiant = models.CharField(max_length=150)
+    offre = models.ForeignKey(Offre, on_delete=models.CASCADE)
+    date_candidature = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nom_etudiant} → {self.offre}"
+
