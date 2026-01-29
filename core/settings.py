6aef10bdd0d621bf66cceec6b44effea2806c8da
chaimae.chidmi <chaimaechidmi@gmail.com>
2026-01-29 23:02:32 +0100

    Add 'stages' app to installed apps

--- core/settings.py
+++ core/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+     "stages",
 
     # Apps ديالكم (زيدوهم من بعد)
     # "users",
