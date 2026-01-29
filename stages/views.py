4677fef4268dd4bc0705bfc0228a82fb53780908
chaimae.chidmi <chaimaechidmi@gmail.com>
2026-01-29 23:33:55 +0100

    Refactor offres_list view to use select_related

--- stages/views.py
+++ stages/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from .models import Offre
+
+def offres_list(request):
+    offres = Offre.objects.select_related("entreprise").all().order_by("-id")
+    return render(request, "stages/offres_list.html", {"offres": offres})
