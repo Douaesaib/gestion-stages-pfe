8745b6947dee51069b78cb6ea5b69ff6afece116
chaimae.chidmi <chaimaechidmi@gmail.com>
2026-01-30 00:26:27 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from stages.views import offres_list
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", offres_list, name="home"),      #  الصفحة الرئيسية
-    path("offres/", offres_list, name="offres_list"),
+    path("", include("stages.urls")),
 ]
