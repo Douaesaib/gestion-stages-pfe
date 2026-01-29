8812b06c69de774cba0a5427122dc40cb7e6c1bd
chaimae.chidmi <chaimaechidmi@gmail.com>
2026-01-29 23:14:42 +0100

    Register models in Django admin

--- stages/admin.py
+++ stages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Entreprise, Offre, Candidature
 
-# Register your models here.
+admin.site.register(Entreprise)
+admin.site.register(Offre)
+admin.site.register(Candidature)
