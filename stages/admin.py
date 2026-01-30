0d3ef77241f2855ec3179e038e2b7c6b3863b337
chaimae.chidmi <chaimaechidmi@gmail.com>
2026-01-30 03:30:45 +0100

    Refactor admin registration for models with custom admin

--- stages/admin.py
+++ stages/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Entreprise, Offre, Candidature
 
-admin.site.register(Entreprise)
-admin.site.register(Offre)
-admin.site.register(Candidature)
+
+@admin.register(Entreprise)
+class EntrepriseAdmin(admin.ModelAdmin):
+    list_display = ("nom", "secteur")
+    search_fields = ("nom", "secteur")
+
+
+@admin.register(Offre)
+class OffreAdmin(admin.ModelAdmin):
+    list_display = ("titre", "entreprise", "date_debut", "date_fin")
+    list_filter = ("entreprise",)
+    search_fields = ("titre", "entreprise__nom")
+
+
+@admin.register(Candidature)
+class CandidatureAdmin(admin.ModelAdmin):
+    list_display = ("nom_stagiaire", "offre", "date_candidature")
+    list_filter = ("offre",)
+    search_fields = ("nom_stagiaire",)
