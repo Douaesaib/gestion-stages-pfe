af6c49256d9153d95d14b80c45951725abb62d2b
chaimae.chidmi <chaimaechidmi@gmail.com>
2026-02-04 23:12:57 +0100

    Update views.py

--- stages/views.py
+++ stages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
+
 from .models import Offre, Candidature
 
 
@@ -12,19 +13,51 @@ def postuler(request, offre_id):
     offre = get_object_or_404(Offre, id=offre_id)
 
     if request.method == "POST":
-        nom = request.POST.get("nom_stagiaire", "").strip()
+        nom = (request.POST.get("nom_stagiaire") or "").strip()
+        prenom = (request.POST.get("prenom_stagiaire") or "").strip()
+        email = (request.POST.get("email") or "").strip()
+        telephone = (request.POST.get("telephone") or "").strip()
+        cv = request.FILES.get("cv")  # ملف ال CV
 
         if not nom:
             messages.error(request, "Veuillez saisir votre nom.")
             return render(request, "stages/postuler.html", {"offre": offre})
 
-        # منع نفس الاسم يترشح لنفس العرض مرة أخرى
-        existe_deja = Candidature.objects.filter(offre=offre, nom_stagiaire=nom).exists()
+        if not prenom:
+            messages.error(request, "Veuillez saisir votre prénom.")
+            return render(request, "stages/postuler.html", {"offre": offre})
+
+        if not email:
+            messages.error(request, "Veuillez saisir votre email.")
+            return render(request, "stages/postuler.html", {"offre": offre})
+
+        if not telephone:
+            messages.error(request, "Veuillez saisir votre téléphone.")
+            return render(request, "stages/postuler.html", {"offre": offre})
+
+        if not cv:
+            messages.error(request, "Veuillez uploader votre CV (PDF).")
+            return render(request, "stages/postuler.html", {"offre": offre})
+
+        existe_deja = Candidature.objects.filter(
+            offre=offre,
+            nom_stagiaire=nom,
+            prenom_stagiaire=prenom
+        ).exists()
+
         if existe_deja:
             messages.warning(request, "Vous avez déjà postulé à cette offre.")
             return redirect("stages:offres_list")
 
-        Candidature.objects.create(offre=offre, nom_stagiaire=nom)
+        Candidature.objects.create(
+            offre=offre,
+            nom_stagiaire=nom,
+            prenom_stagiaire=prenom,
+            email=email,
+            telephone=telephone,
+            cv=cv
+        )
+
         messages.success(request, "Votre candidature a été envoyée ✅")
         return redirect("stages:offres_list")
 
