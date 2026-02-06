05790e393ae41d4694236fcd1f080c1ec70a432b
AYAA <benajibayaya@gmail.com>
2026-02-06 21:40:06 +0100

    ajoute models et design

--- pages/models.py
+++ pages/models.py
@@ -2,13 +2,22 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    # Bach n-ferqo bin l-étudiant w l-prof
-    is_stagiaire = models.BooleanField(default=False)
-    is_encadrant = models.BooleanField(default=False)
+    ROLE_CHOICES = (
+        ('ADMIN', 'Admin'),
+        ('ETUDIANT', 'Etudiant'),
+        ('ENTREPRISE', 'Entreprise'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
-class Stagiaire(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Etudiant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, limit_choices_to={'role': 'ETUDIANT'})
     cne = models.CharField(max_length=20)
     filiere = models.CharField(max_length=100)
     telephone = models.CharField(max_length=15)
+    cv = models.FileField(upload_to='cvs/', null=True, blank=True)
 
+class Entreprise(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, limit_choices_to={'role': 'ENTREPRISE'})
+    nom_entreprise = models.CharField(max_length=100)
+    ville = models.CharField(max_length=50)
+    ice = models.CharField(max_length=20)
