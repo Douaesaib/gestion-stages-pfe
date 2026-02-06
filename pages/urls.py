9787f12b23a6cc2dddaf0cf0bd1731bf55409d49
AYAA <benajibayaya@gmail.com>
2026-02-06 22:49:53 +0100

    modification

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'), # Hada hoa l-jdid!
+    path('profil/', views.modifier_profil, name='modifier_profil')
 ]
