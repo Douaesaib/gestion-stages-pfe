6eb72c7cc779bc73206ea47e690e824fb8af9faa
AYAA <benajibayaya@gmail.com>
2026-02-23 11:35:15 +0000

    add import

--- pages/forms.py
+++ pages/forms.py
@@ -15,11 +15,10 @@ class EtudiantProfileForm(forms.ModelForm):
         fields = ['cne', 'filiere', 'telephone', 'ecole', 'niveau_etudes', 'competences', 'experiences', 'projets', 'cv_file']
         widgets = {
             'cne': forms.TextInput(attrs={'class': 'form-control'}),
-            'filiere': forms.TextInput(attrs={'class': 'form-control'}),
             'telephone': forms.TextInput(attrs={'class': 'form-control'}),
             'ecole': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ecole / Université'}),
             'filiere': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Filière'}),
-            'competences': forms.TextInput(attrs={'class': 'form-control', 'id': 'id_competences', 'placeholder': 'Python, Java, SQL...'}),
+            'competences': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Python, Java, SQL...'}),
             'projets': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Description de vos projets'}),
             'experiences': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Stages ou expériences passées'}),
             'cv_file': forms.FileInput(attrs={'class': 'form-control'}),
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, authenticate
 from django.contrib.auth.decorators import login_required
-from .forms import EtudiantProfileForm , SignUpForm
+from .forms import EtudiantProfileForm , EntrepriseProfileForm, SignUpForm
 from .models import Etudiant,Entreprise
 
 
